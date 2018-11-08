e8b54345e60bb769bf8f16f5bcec4786a83edaa1
Felipe Megale <felipe.megale17@gmail.com>
2018-11-08 00:54:38 +0000

    Begin board implementation

---
+++ battleship/board.py
@@ -0,0 +1,59 @@
+'''
+Board is 10 x 10
+Boats:
+    Carrier: 5 blocks (1 per board)
+    Tanker: 4 blocks (2 per board)
+    Destroyer: 3 blocks (3 per board)
+    Submarine: 2 blocks (4 per board)
+
+'''
+class Board:
+
+    def print_board(self):
+        for _list in self.board:
+            for el in _list:
+                print("%c " %(el), end = '')
+            print("")
+
+    def verify_place(self, boat_type):
+        # check if is possible to add boat
+        print("verify_place")
+
+    def place_carrier(self):
+        # choose random row and column
+        print("place_carrier")
+    
+    def place_tanker(self):
+        # choose random row and column
+        print("place_tanker")
+    
+    def place_destroyer(self):
+        # choose random row and column
+        print("place_destroyer")
+
+    def place_submarine(self):
+        # choose random row and column
+        print("place_submarine")
+    
+    # server's board assembly
+    def assemble_random_board(self):
+        self.place_carrier()
+        self.place_tanker()
+        self.place_tanker()
+        self.place_destroyer()
+        self.place_destroyer()
+        self.place_destroyer()
+        self.place_submarine()
+        self.place_submarine()
+        self.place_submarine()
+        self.place_submarine()
+
+    def __init__(self):
+        self.board = [['e' for i in range(10)] for j in range(10)]
+
+def main():
+    board = Board()
+    board.print_board()
+
+if __name__ == '__main__':
+    main()

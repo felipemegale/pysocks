743ec8aa0fc88b2b65e1b2208ca6de317bf2f83d
Felipe Megale <felipe.megale17@gmail.com>
2018-11-09 12:55:03 +0000

    Placing ships

--- battleship/board.py
+++ battleship/board.py
@@ -2,10 +2,10 @@ import random
 '''
 Board is 10 x 10
 Boats:
-    Carrier: 5 blocks (1 per board)
-    Tanker: 4 blocks (2 per board)
-    Destroyer: 3 blocks (3 per board)
-    Submarine: 2 blocks (4 per board)
+    Carrier: 5 blocks (1 per board) - vertical
+    Tanker: 4 blocks (2 per board) - horizontal
+    Destroyer: 3 blocks (3 per board) - vertical
+    Submarine: 2 blocks (4 per board) - horizontal
 
 e e e e e e e e e e
 e e e e e e e e e e
@@ -26,45 +26,26 @@ class Board:
                 print("%c " %(el), end = '')
             print("")
 
-    def verify_place(self, boat_type):
-        # check if is possible to add boat
-        print("verify_place")
+    def get_origin(self):
+        return random.randint(0,9), random.randint(0,9)
 
     def place_carrier(self):
-        # choose random row and column
-        print("place_carrier")
+        i,j = self.get_origin()
 
-        # generate random position on the board
-        i = random.randint(0,9)
-        j = random.randint(0,9)
-
-        if self.board[i][j] == 'e':
-            
-
-    def place_tanker(self):
-        # choose random row and column
-        print("place_tanker")
+        if i >= 5:
+            for _i in range(5, 10):
+                self.board[_i][j] = 's'
+        else:
+            for _i in range(i, i+5):
+                self.board[_i][j] = 's'
     
-    def place_destroyer(self):
-        # choose random row and column
-        print("place_destroyer")
-
-    def place_submarine(self):
-        # choose random row and column
-        print("place_submarine")
+    def place_tankers(self):
+        print("placed tankers")
     
     # server's board assembly
     def assemble_random_board(self):
         self.place_carrier()
-        self.place_tanker()
-        self.place_tanker()
-        self.place_destroyer()
-        self.place_destroyer()
-        self.place_destroyer()
-        self.place_submarine()
-        self.place_submarine()
-        self.place_submarine()
-        self.place_submarine()
+        self.place_tankers()
 
     def __init__(self):
         self.board = [['e' for i in range(10)] for j in range(10)]
@@ -73,6 +54,8 @@ def main():
     board = Board()
     board.print_board()
     board.assemble_random_board()
+    print("\n")
+    board.print_board()
 
 if __name__ == '__main__':
     main()

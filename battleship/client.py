df4b0d72840b468c9092978ef13b027ce0d28862
Felipe Megale <felipe.megale17@gmail.com>
2018-11-13 19:45:39 +0000

    Started client board assembly

--- battleship/client.py
+++ battleship/client.py
@@ -1,10 +1,31 @@
 import socket
 
+class client(object):
+
+    def print_board(self):
+        for _list in self.board:
+            for el in _list:
+                print("%c " %(el), end = '')
+            print("")
+
+    def assemble_board(self):
+        assembly_file = open("user_board.txt", "r")
+
+        content = assembly_file.readlines()
+        content = [line.strip() for line in content]
+
+        assembly_file.close()
+
+    def __init__(self):
+        self.board = [['e' for i in range(10)] for j in range(10)]
+        self.assemble_board()
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 port = 1234
 
 tcp.connect(('127.0.0.1', port))
+print("Conectou ao servidor")
 
 print("Para sair use CTRL+X\n")
 

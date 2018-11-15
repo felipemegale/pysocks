86b4b12e6b7b60934c15dc85fd4bdc028c7a288c
Felipe Megale <felipe.megale17@gmail.com>
2018-11-15 00:30:14 +0000

    Improved playability

--- battleship/client.py
+++ battleship/client.py
@@ -28,6 +28,7 @@ class client(object):
     def __init__(self):
         self.board = [['e' for i in range(10)] for j in range(10)]
         self.assemble_board()
+        self.ship_qty = 30
 
 s_board = [['?' for i in range(10)] for j in range(10)]
 
@@ -52,25 +53,40 @@ def main():
     msg = input("~ ")
 
     while msg != 'stop':
-        if msg == 'p':
+        if msg == 'p' or msg == 'P':
             print_server_board()
-        elif msg == 'h':
+        elif msg == 'h' or msg == 'H':
             print("Entre 'p' para ver o que aprendeu do servidor")
             print("Entre 'stop' para sair")
             print("Para atacar, informe <linha>,<coluna>. E.g. A,1")
         else:
             tcp.send(bytes(msg, 'utf-8'))
             
-            msg_from_server = '<server> ' + tcp.recv(2048).decode('utf-8')
+            msg_from_server = tcp.recv(2048).decode('utf-8')
             print(msg_from_server)
-            
-            msg_from_server = msg_from_server.split(' ') # separate <server> from atk position
-            msg_from_server = msg_from_server[1].split(',')
-            
-            lin = ord(msg_from_server[0])-65
-            col = ord(msg_from_server[1])-1
-            
-            print(lin, col)
+
+            msg_from_server = msg_from_server.split(',')
+
+            if msg_from_server[0] == 'hit':
+                # process clients atk
+                msg = msg.split(',')
+                row = ord(msg[0]) - 65
+                col = int(msg[1]) - 1
+
+                s_board[row][col] = 'H'
+
+                # process servers atk
+                row = ord(msg_from_server[1]) - 65
+                col = int(msg_from_server[2]) - 1
+
+                if (cli.board[row][col] != 'e'):
+                    cli.board[row][col] = 'X'
+                    cli.ship_qty -= 1
+
+            elif msg_from_server[0] == 'miss':
+                msg = msg.split(',')
+                s_board[ord(msg[0])-65][int(msg[1])-1] = 'm'
+
         msg = input("~ ")
         
     tcp.close()
--- battleship/server.py
+++ battleship/server.py
@@ -1,5 +1,5 @@
 import socket
-from Board import Board	
+from Board import *
 
 def main():
 	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,12 +26,23 @@ def main():
 			
 			msg_from_cli = msg_from_cli.split(',') # clients atk
 			a_row = ord(msg_from_cli[0])-65
-			a_col = msg_from_cli[1]-1
+			a_col = int(msg_from_cli[1])-1
 
 			# test clients atk on server's board
-			if s_board.board[a_row][a_col]
+			# if that position in the board is not empty, means that the client has hit a ship
+			if (s_board.board[a_row][a_col] != 'e'):
+				s_board.board[a_row][a_col] = 'X'
+				_i = random.randint(0,9)
+				_j = random.randint(0,9)
+				s_atk = "hit" + "," + _i + "," + _j
+				conn.send(bytes(s_atk,'utf-8'))
+			else:
+				_i = random.randint(0,9)
+				_j = random.randint(0,9)
+				s_atk = "miss" + "," + _i + "," + _j
+				conn.send(bytes("miss",'utf-8'))
 
-			conn.send(bytes(msg_to_cli, 'utf-8')) # send something back to the client
+			#conn.send(bytes(msg_to_cli, 'utf-8')) # send something back to the client
 
 		print("End connection with", addr)	
 		conn.close()

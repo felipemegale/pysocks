fe2dd8ceae61cbdd3f83da55010ef3275f2a8639
Felipe Megale <felipe.megale17@gmail.com>
2018-11-19 15:47:46 +0000

    UDP comm works

--- Pricing_System/client.py
+++ Pricing_System/client.py
@@ -1,37 +1,38 @@
 import socket
 
-class client(object):
+# class client(object):
 
-    #def print_price(self):
-    #    for _list in self.price:
-    #        for el in _list:
-    #            print("%c " %(el), end = '')
-    #        print("")
+#     #def print_price(self):
+#     #    for _list in self.price:
+#     #        for el in _list:
+#     #            print("%c " %(el), end = '')
+#     #        print("")
 
-    def assemble_price(self):
-        assembly_file = open("user_price.txt", "r")
+#     def assemble_price(self):
+#         assembly_file = open("user_price.txt", "r")
 
-        content = assembly_file.readlines()
-        content = [line.strip() for line in content]
+#         content = assembly_file.readlines()
+#         content = [line.strip() for line in content]
 
-        assembly_file.close()
+#         assembly_file.close()
 
-    def __init__(self):
-        self.assemble_price()
+#     def __init__(self):
+#         self.assemble_price()
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 port = 1234
 
-udp.connect(('127.0.0.1', port))
-print("Conectou ao servidor")
+dest = ('127.0.0.1', 1234)
 
 print("Para sair use CTRL+X\n")
 
 msg = input()
 
-while msg != 'stop':
-    udp.send(bytes(msg, 'utf-8'))
-    msg = input()
-
-udp.close()
+while True:
+    if msg != "stop":
+        udp.sendto(bytes(msg, "utf-8"), dest)
+        msg = input()
+    else:
+        udp.close()
+        break
--- Pricing_System/server.py
+++ Pricing_System/server.py
@@ -1,5 +1,5 @@
 import socket
-from Price import Price
+#from Price import Price
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("Socket created successfully")
@@ -9,10 +9,14 @@ port = 1234
 udp.bind(('', port))
 print("Socket bound to %s" %(port))
 
-#udp.listen(1)
 print("Socket is listening")
 
 while True:
 	msg, client = udp.recvfrom(1024)
-	print client, msg
-udp.close()
+	msg = msg.decode("utf-8")
+
+	if msg != "stop":
+		print (client, msg)
+	else:
+		udp.close()
+		break

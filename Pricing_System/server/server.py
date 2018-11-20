8e859558b82d27d3d788082b5071b5ca6c9d81c3
Felipe Megale <felipe.megale17@gmail.com>
2018-11-20 20:47:21 +0000

    Start processing messages

--- Pricing_System/server/server.py
+++ Pricing_System/server/server.py
@@ -1,6 +1,8 @@
 import socket
 #from Price import Price
 
+stations = []
+
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("Socket created successfully")
 
@@ -12,12 +14,30 @@ print("Socket bound to %s" %(port))
 print("Socket is listening")
 
 while True:
-	msg, client = udp.recvfrom(1024) # message is data or search
-	msg = msg.decode("utf-8")
+	msg, client = udp.recvfrom(1024) # message is data (D) or search (P)
+	msg_decode = msg.decode("utf-8") 
+	msg_split = msg_decode.split("-")
+	d_or_p = msg_split[0].upper()
 
-	if msg != "stop":
-		print (client, msg)
-		udp.sendto(bytes(msg, "utf-8"), client)
+	# if client is not disconnecting...
+	if msg_split[0] != "stop":
+		
+		# if user is inputting data...
+		if d_or_p == "D":
+			udp.sendto(bytes(True, "utf-8"), client)
+			data_file = open("server_data.txt", "a")
+			data_file.write(msg_decode)
+			data_file.close()
+		
+		# if the user is searching...
+		elif d_or_p == "P":
+			msg_id = msg_split[1]
+			fuel = msg_split[2]
+			radius = msg_split[3]
+			lat = msg_split[4]
+			lon = msg_split[5]
+
+		
 	else:
 		udp.close()
 		break
